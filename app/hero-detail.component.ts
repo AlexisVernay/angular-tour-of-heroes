db8d39ebcc123b34ae7a6ce22f9e8b57b411be1d
Vernay Alexis <Alexis.Vernay@e.ujf-grenoble.fr>
2016-12-13 15:42:43 +0100

    Etape 5 du tutoriel Angulaire 2 => Tour of Heroes

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
+
 import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
   template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} détails!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>nom: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
+    <div *ngIf="hero">
+      <h2>{{hero.name}} détails!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>nom: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+        </div>
+      </div>
+  `
 })
 
 export class HeroDetailComponent {
